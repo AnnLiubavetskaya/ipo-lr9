594fabdf92fc30c80f0c36d1b9db8a838b41ddd5
AnnLiubavetskaya <20anita07l@gmail.com>
2024-12-17 15:31:57 +0300

    Task 3 complete

--- collision/rect.py
+++ collision/rect.py
@@ -7,3 +7,25 @@ def isCorrectRect(spisok):
         return True
     else:
         return False
+
+def isCollisionRect(spisok1, spisok2):
+    if not isCorrectRect(spisok1):
+        raise ValueError("1й прямоугольник некорректный")
+    if not isCorrectRect(spisok2):
+        raise ValueError("2й прямоугольник некорректный")
+    kortezh1 = spisok1[0]
+    kortezh2 = spisok1[1]
+    kortezh3 = spisok2[0]
+    kortezh4 = spisok2[1]
+    x_1_minimalny = kortezh1[0]
+    y_1_minimalny = kortezh1[1]
+    x_1_maksimalny = kortezh2[0]
+    y_1_maksimalny = kortezh2[1]
+    x_2_minimalny = kortezh3[0]
+    y_2_minimalny = kortezh3[1]
+    x_2_maksimalny = kortezh4[0]
+    y_2_maksimalny = kortezh4[1]
+    if x_1_maksimalny < x_2_minimalny or x_1_minimalny > x_2_maksimalny or y_1_maksimalny < y_2_minimalny or y_1_minimalny > y_2_maksimalny:
+        return False
+    else:
+        return True
